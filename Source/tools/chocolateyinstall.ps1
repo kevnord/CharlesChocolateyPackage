1b2da94dd79d2548ca649f2c290efe372b4d4717
Kevin Nord <knord@meredith.com>
2017-04-20 09:16:20 -0700

    Updated to 4.1

--- Source/tools/chocolateyinstall.ps1
+++ Source/tools/chocolateyinstall.ps1
@@ -1,22 +1,27 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$ErrorActionPreference = 'Stop'; # stop on all errors
+
+
+$packageName= 'Charles' # arbitrary name for the package, used in messages
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.charlesproxy.com/assets/release/4.1/charles-proxy-4.1-win32.msi'
+$url64bit   = 'https://www.charlesproxy.com/assets/release/4.1/charles-proxy-4.1-win64.msi'
 
 $packageArgs = @{
-  packageName   = 'Charles' # arbitrary name for the package, used in messages
+  packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'msi' #only one of these: exe, msi, msu
-  url           = '{{Url32}}'
-  url64bit      = '{{Url64}}'
+  url           = $url
+  url64bit      = $url64
 
-  #MSI
-  silentArgs    = "/qn /norestart"
-  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/S'           # NSIS
+  validExitCodes= @(0) #please insert other valid exit codes here
 
-  softwareName  = 'Charles*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum      = '{{Checksum32}}'
-  checksumType  = 'sha1'
-  checksum64    = '{{Checksum64}}'
-  checksumType64= 'sha1'
+  # optional, highly recommended
+  softwareName  = 'Charles' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  checksum      = ''
+  checksumType  = 'md5' #default is md5, can also be sha1
+  checksum64    = ''
+  checksumType64= 'md5' #default is checksumType
 }
 
 Install-ChocolateyPackage @packageArgs
